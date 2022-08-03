03b20665a894cadaebff6c799a3d7ce82c18af96
youngkyo0504 <keumky2@gmail.com>
2022-08-03 17:38:36 +0900

    feat: env validation 적용 #7
    - joi 설치
    - port , nodeenv validation

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from './config/configuration';
+import validationSchema from './config/validationScheme';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [configuration],
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV ?? 'development'}`,
+      validationSchema,
     }),
   ],
   controllers: [AppController],
