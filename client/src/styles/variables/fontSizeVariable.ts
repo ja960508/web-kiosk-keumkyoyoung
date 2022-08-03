315cbb78427294984c55b97a6039562f19243b92
youngkyo0504 <keumky2@gmail.com>
2022-08-03 15:20:33 +0900

    feat: css variable 작성 #2
    - borderRadius variable
    - color variable (나중에 더 추가)
    - font size
    - shadowVariable
    - spaceVariable

---
+++ client/src/styles/variables/fontSizeVariable.ts
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+const fontSizeVariable = css`
+  --text-xs: 12px;
+  --text-sm: 14px;
+  --text-md: 16px;
+  --text-lg: 18px;
+  --text-xl: 24px;
+  --text-2xl: 32px;
+`;
+
+export default fontSizeVariable;
