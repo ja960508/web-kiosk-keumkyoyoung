600ceb955318e7ea16817d7686073336a39470c0
youngkyo0504 <keumky2@gmail.com>
2022-08-06 22:22:10 +0900

    feat menu service, menuItem Service 작성  #12
    - /menu 로 겟 요청시 data 요청 가능

--- server/src/menu/menu.service.ts
+++ server/src/menu/menu.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMenuDto } from './dto/create-menu.dto';
-import { UpdateMenuDto } from './dto/update-menu.dto';
+import { MenuRepository } from './repository/menu.repository';
 
 @Injectable()
 export class MenuService {
+  constructor(private readonly menuRepository: MenuRepository) {}
   create(createMenuDto: CreateMenuDto) {
     return 'This action adds a new menu';
   }
 
-  findAll() {
-    return `This action returns all menu`;
+  async findAll() {
+    return await this.menuRepository.findAll();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} menu`;
-  }
-
-  update(id: number, updateMenuDto: UpdateMenuDto) {
-    return `This action updates a #${id} menu`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} menu`;
+    return this.menuRepository.findById(id);
   }
 }
