4c0e76af1275e10c88e0136254843fd17f4feeb1
youngkyo0504 <keumky2@gmail.com>
2022-08-07 12:21:07 +0900

    feat: order api작성 완료 #14
    - sql에서 korea 시간으로 맞춰주기 위해서 typeorm설정을 변경

--- server/src/order/order.controller.ts
+++ server/src/order/order.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { OrderService } from './order.service';
-import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrderDto } from './dto/order.dto';
 
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
+  create(@Body() createOrderDto: OrderDto) {
     return this.orderService.create(createOrderDto);
   }
 
