5e2c18beb5aba37c94f4f21a4080a093a3c6dc4b
youngkyo0504 <keumky2@gmail.com>
2022-08-10 01:25:36 +0900

    style: menu 관련 컴포넌트 하나의 폴더로 관리 #27

--- client/src/components/Menu/MenuList.tsx
+++ client/src/components/Menu/MenuList.tsx
@@ -1,18 +1,15 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import mixin from '../../styles/mixin';
+import { useMenuData } from '.';
 import MenuItem from './MenuItem';
 
-// interface MenuListProps {}
-
-const MenuList: FC = ({}) => {
-  const arr = new Array(40).fill(0);
+const MenuList: FC = () => {
+  const { selectedMenuItems } = useMenuData();
   return (
     <Wrapper>
       <MenuListWrapper>
-        {arr.map((a, index) => (
-          <MenuItem key={index} />
-        ))}
+        {selectedMenuItems &&
+          selectedMenuItems.menuItem.map((item) => <MenuItem key={item.id} menuItemData={item} />)}
       </MenuListWrapper>
     </Wrapper>
   );
@@ -21,11 +18,15 @@ const MenuList: FC = ({}) => {
 export default MenuList;
 
 const MenuListWrapper = styled.ul`
-  ${mixin.flexMixin({ wrap: 'wrap', justify: 'space-around' })}
+  display: grid;
+  min-width: 100%;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
   row-gap: var(--space-5);
   column-gap: var(--space-6);
 `;
 
 const Wrapper = styled.div`
   overflow-y: hidden;
+  width: 100%;
 `;
