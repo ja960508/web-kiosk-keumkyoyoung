e9baad599ca2578a6c56bed48d1cb58a48c24397
youngkyo0504 <keumky2@gmail.com>
2022-08-10 23:52:43 +0900

    feat: component를 받을 수 있는 ref를 추가 #29
    ㅡ props로 전달받은 animateRef는 shared animation에 사용됨

--- client/src/lib/animation/animationComponent.tsx
+++ client/src/lib/animation/animationComponent.tsx
@@ -1,49 +1,54 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useRef } from 'react';
+import { getSharedAnimationKeyFrame } from '../../util/animate';
 import { usePresence } from './PresenceChild';
 type AnimatedKeyFrame = Keyframe & {
   [key in keyof React.CSSProperties]: string;
 };
 
-interface AnimatedComponentProps<T> extends React.HTMLProps<T> {
+interface AnimatedComponentProps<T extends HTMLElement>
+  extends React.DetailedHTMLProps<React.HTMLAttributes<T>, T> {
   onExit?: AnimatedKeyFrame[];
   onEnter?: AnimatedKeyFrame[];
   children: React.ReactNode;
   isVisible?: boolean;
+  animatedRef?: React.RefObject<T>;
   keyframeOption?: KeyframeAnimationOptions | undefined;
   onExitAnimationDone?: () => void;
 }
 
-function AnimatedComponent(Tag: keyof React.ReactHTML) {
+function AnimatedComponent<T extends HTMLElement>(Tag: keyof React.ReactHTML) {
   return function AnimatedComponent({
     onExit = [{}],
     onEnter = [{}],
     keyframeOption,
     children,
+    animatedRef,
     ...otherProps
-  }: AnimatedComponentProps<React.ReactHTML>) {
+  }: AnimatedComponentProps<T>) {
     const { isVisible, onExitAnimationDone } = usePresence();
 
     const elementRef = useRef<HTMLElement>(null);
-
     const defaultKeyFrameOption: KeyframeAnimationOptions = {
-      duration: 700,
+      duration: 500,
       fill: 'forwards',
       easing: 'ease-in-out',
     };
 
     useEffect(() => {
       if (!elementRef.current) return;
+      const sharedKeyframe = getSharedAnimationKeyFrame(animatedRef, elementRef);
       if (isVisible) {
-        const animation = elementRef.current.animate(onEnter, {
+        const animation = elementRef.current.animate(sharedKeyframe || onEnter, {
           ...defaultKeyFrameOption,
           ...keyframeOption,
         });
 
         return () => animation.cancel();
       } else {
-        const animation = elementRef.current.animate(onExit, {
+        const animation = elementRef.current.animate(sharedKeyframe || onExit, {
           ...defaultKeyFrameOption,
+          direction: sharedKeyframe ? 'reverse' : 'normal',
           ...keyframeOption,
         });
         animation.commitStyles();
